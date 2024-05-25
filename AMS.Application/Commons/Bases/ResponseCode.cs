be24012f1fa5a37d7893bb1baa59293e344218ee
AdrianoGongora <adriano.gongora.juarez@gmail.com>
2024-05-24 23:21:57 -0500

    feat: Alter Validation response error

--- AMS.Application/Commons/Bases/ResponseCode.cs
+++ AMS.Application/Commons/Bases/ResponseCode.cs
@@ -1,9 +1,15 @@
 ﻿namespace AMS.Application.Commons.Bases
 {
+    //No le veo sentido dar un status code si de por si con un boolean puedo decirte si salio bien o mal el resultado
     public enum ResponseCode
     {
         OK = 200,
-        EROOR = 404,
+        CREATED = 201,
+        ACCEPTED = 202,
+        NOT_CONTENT = 204,
+        BAD_REQUEST = 400,
+        UNAUTHORIZED = 401,
         FORBIDDEN = 403,
+        NOT_FOUND = 404,
     }
 }
