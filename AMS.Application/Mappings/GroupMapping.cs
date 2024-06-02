3a30d3922270ad9318b5652e541141ce48385779
AlambrezFE <andre29flores.aga.nst@gmail.com>
2024-06-02 11:49:31 -0500

    feat: Creacion de mapping eliminar grupo

---
+++ AMS.Application/Mappings/GroupMapping.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using AMS.Application.UseCases.Group.Command.CreateGroup;
+using AMS.Application.UseCases.Group.Command.UpdateGruop;
+using AMS.Domain.Entities;
+using AMS.Application.UseCases.Group.Command.DeleteGruop;
+using AMS.Application.Dtos.Groups;
+
+namespace AMS.Application.Mappings;
+
+public class GroupMapping : Profile
+{
+    public GroupMapping()
+    {
+        CreateMap<DeleteGroupCommand, Group>();
+    }
+}
