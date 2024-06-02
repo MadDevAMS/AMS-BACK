5c400b78e9c1d9c768bdb0dea47d470a33d0bf7c
AlambrezFE <andre29flores.aga.nst@gmail.com>
2024-06-02 11:51:53 -0500

    feat: creacion de caso de uso de "eliminacion" de group

--- AMS.Application/Interfaces/Persistence/IGroupRepository.cs
+++ AMS.Application/Interfaces/Persistence/IGroupRepository.cs
@@ -1,6 +1,11 @@
-﻿namespace AMS.Application.Interfaces.Persistence
+﻿using AMS.Application.Dtos.Groups;
+using AMS.Domain.Entities;
+
+namespace AMS.Application.Interfaces.Persistence
 {
     public interface IGroupRepository
     {
+        Task DeleteAsync(long groupId);
+
     }
 }
---
+++ AMS.Application/UseCases/Group/Command/DeleteGroup/DeleteGroupCommand.cs
@@ -0,0 +1,11 @@
+﻿using AMS.Application.Commons.Bases;
+using MediatR;
+
+namespace AMS.Application.UseCases.Group.Command.DeleteGruop
+{
+    public class DeleteGroupCommand : IRequest<BaseResponse<bool>>
+    {
+        public long Id { get; set; }
+
+    }
+}
