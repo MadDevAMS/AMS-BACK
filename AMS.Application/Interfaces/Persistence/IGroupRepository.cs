2675a18bdd501a5aa422c48f0272d590418fb38d
AlambrezFE <andre29flores.aga.nst@gmail.com>
2024-06-02 10:38:08 -0500

    feat: interface Group Repository.

--- AMS.Application/Interfaces/Persistence/IGroupRepository.cs
+++ AMS.Application/Interfaces/Persistence/IGroupRepository.cs
@@ -1,6 +1,19 @@
-﻿namespace AMS.Application.Interfaces.Persistence
+﻿using AMS.Application.Dtos.Groups;
+using AMS.Domain.Entities;
+
+namespace AMS.Application.Interfaces.Persistence
 {
     public interface IGroupRepository
     {
+        Task CreateAsync(GroupsDto group);
+
+        // ---------------------------------
+        Task CreateWithUserAsync(Group group);
+        Task UpdateAsync(Group group);
+        Task DeleteAsync(long groupId);
+
+        // ---------------------------------
+
+        Task<long> GroupExistsAsync(string groupName, long idEntidad);
     }
 }
