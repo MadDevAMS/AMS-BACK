7ad9c23b9851f57af3e40c8590c6dc6fe1c25131
AdrianoGongora <adriano.gongora.juarez@gmail.com>
2024-06-13 22:46:47 -0500

    refact: Remove name aws credentials

--- AMS.Infrastructure/Commons/AWS/AwsCredentials.cs
+++ AMS.Infrastructure/Commons/AWS/AwsCredentials.cs
@@ -2,7 +2,7 @@
 {
     public class AwsCredentials
     {
-        public const string AwsPK = "AKIA2UC3CT3UIWN5OWWW";
-        public const string AwsSK = "/pB+3ofLDtOtl0LvpTfdZ++G1omZvjWwAw2G3Saq";
+        public const string AwsPublicKey = "";
+        public const string AwsSecretKey = "";
     }
 }
