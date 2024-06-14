a79d127a3ab2328a5efc2032cf17b3b44f565686
AdrianoGongora <adriano.gongora.juarez@gmail.com>
2024-06-14 10:41:49 -0500

    refact: Alter Group idEntidad nullable true

--- AMS.Infrastructure/Persistence/Migrations/20240607023909_Initial_Migrations.cs
+++ AMS.Infrastructure/Persistence/Migrations/20240607023909_Initial_Migrations.cs
@@ -94,7 +94,7 @@ namespace AMS.Infrastructure.Persistence.Migrations
                         .Annotation("SqlServer:Identity", "1, 1"),
                     Name = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
                     Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    IdEntidad = table.Column<long>(type: "bigint", nullable: false),
+                    IdEntidad = table.Column<long>(type: "bigint", nullable: true),
                     State = table.Column<int>(type: "int", nullable: false),
                     AuditCreateUser = table.Column<long>(type: "bigint", nullable: true),
                     AuditCreateDate = table.Column<DateTime>(type: "datetime2", nullable: false),
