8cbeccb76c73d5b4d28266422c8cdc5d6fb91192
AdrianoGongora <adriano.gongora.juarez@gmail.com>
2024-06-13 22:42:54 -0500

    refact: chage name aws credentials

--- AMS.Infrastructure/Commons/AWS/AwsCredentials.cs
+++ AMS.Infrastructure/Commons/AWS/AwsCredentials.cs
@@ -2,7 +2,7 @@
 {
     public class AwsCredentials
     {
-        public const string AwsPublicKey = "AKIA2UC3CT3UIWN5OWWW";
-        public const string AwsSecretKey = "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";
+        public const string AwsPK = "AKIA2UC3CT3UIWN5OWWW";
+        public const string AwsSK = "/pB+3ofLDtOtl0LvpTfdZ++G1omZvjWwAw2G3Saq";
     }
 }
